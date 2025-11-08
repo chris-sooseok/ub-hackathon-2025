ca576ec243b3b80b99ed3de13b98a7a24f0b8fa4
chris-sooseok <sooseokkim99@gmail.com>
2025-11-08 14:46:43 -0500

    adding react router scaffolding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"; 
+import RootLayout from './pages/RootLayout';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+
+export const router = createBrowserRouter(
+  [ 
+    { path: "/", element: <Navigate to="/app" replace /> },
+    { path: "/app", 
+      element: <RootLayout />,
+      children: [
+        {index: true, element: <LandingPage />},
+        {path: 'login', element: <LoginPage />}
+      ],
+    },
+    { path: "*", element: <Navigate to="/app" replace /> }, // simple catch-all
+  ]
+)
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
