dca04a55166c59d642cf848ad433ebfccd8ac3ea
chris-sooseok <sooseokkim99@gmail.com>
2025-11-08 16:14:40 -0500

    added basic router scalffolding

--- client/src/pages/RootLayout.jsx
+++ client/src/pages/RootLayout.jsx
@@ -3,33 +3,27 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function RootLayout() {
-    // const [connectionStatus, setConnectionStatus] = useState(null)
+    const [connectionStatus, setConnectionStatus] = useState(null)
     
-    // const checkConnection = async () => {
-    //     try {
-    //       const response = await fetch(`${import.meta.env.VITE_API_URL || ''}/check-connection`)
-    //       if (!response.ok) throw new Error('Network response was not ok')
-    //       const data = await response.json()
-    //       setConnectionStatus(data.message || 'Connected!')
-    //     } catch (error) {
-    //       setConnectionStatus('❌ Connection failed')
-    //       console.error(error)
-    //     }
-    //   }
-
-    // useEffect(() => {
-    //     checkConnection()
-    // }, [])
-
-    const [test, setTest] = useState(false);
+    const checkConnection = async () => {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_API_URL || ''}/check-connection`)
+          if (!response.ok) throw new Error('Network response was not ok')
+          const data = await response.json()
+          setConnectionStatus(data.message || 'Connected!')
+        } catch (error) {
+          setConnectionStatus('❌ Connection failed')
+          console.error(error)
+        }
+      }
 
     useEffect(() => {
-        setTest("test!");
+        checkConnection()
     }, [])
 
     return (
     <>
-        <p>{test}</p>
+        <p>{connectionStatus}</p>
         <Outlet />
     </>
     )
